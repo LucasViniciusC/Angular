a2e92dcff0e6cf68889648db59bfe20073855d59
Lucas Cruz <lvplasey2@hotmail.com>
2021-07-26 22:00:03 -0300

    Atividade 26-07 Generation: Inclusão de componentes

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TelaLoginComponent } from './tela-login/tela-login.component';
+import { TelaCadastroComponent } from './tela-cadastro/tela-cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TelaLoginComponent,
+    TelaCadastroComponent
   ],
   imports: [
     BrowserModule,
