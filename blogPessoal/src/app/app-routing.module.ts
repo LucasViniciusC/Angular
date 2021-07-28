896003452da8ed03c66c8462a0c4155ca8a41e70
Lucas Cruz <lvplasey2@hotmail.com>
2021-07-27 23:28:31 -0300

    [Atividade 03] 27-07 Generation - Adicionando primeiras rotas da tela de login e cadastro

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TelaCadastroComponent } from './tela-cadastro/tela-cadastro.component';
+import { TelaLoginComponent } from './tela-login/tela-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'logar', pathMatch: 'full'},
+  {path: 'logar', component: TelaLoginComponent},
+  {path: 'cadastrar', component: TelaCadastroComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
